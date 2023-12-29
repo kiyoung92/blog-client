8e101e4f1325edadb404ed2f92e5a20921e7c64c
kiyoung92 <go3go1go2@gmail.com>
2023-12-30 03:02:46 +0900

    design: sign up page

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,7 +1,23 @@
-export default function Test() {
+'use client';
+import styles from './sign.up.module.css';
+
+export default function SignUp() {
   return (
-    <main>
-      <h1>TestPage</h1>
-    </main>
+    <div className={styles.contentWrap}>
+      <h1 className={styles.title}>SIGN UP</h1>
+      <div className='underlineInputWrap'>
+        <input type='text' placeholder='이메일을 입력해 주세요.' autoFocus />
+        <p></p>
+      </div>
+      <div className='underlineInputWrap'>
+        <input type='text' placeholder='닉네임을 입력해 주세요.' />
+        <p></p>
+      </div>
+      <div className='underlineInputWrap'>
+        <input type='password' placeholder='패스워드를 입력해 주세요.' />
+        <p></p>
+      </div>
+      <button className={styles.signUpButton}>회원가입</button>
+    </div>
   );
 }
